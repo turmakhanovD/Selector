1f3d27bca089b39d865925988c730646e2eb9b8d
turmakhanovD <46023441+turmakhanovD@users.noreply.github.com>
2019-05-28 18:22:14 +0600

    Add files via upload

---
+++ BigMath/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialAsync();
+          
+        }
+        static void Factorial()
+        {
+            int result = 1;
+            for (int i = 1; i <= 600; i++)
+            {
+                result *= i;
+            }
+        
+        }
+
+        static async void FactorialAsync()
+        {
+  
+            await Task.Run(() => Factorial());                           
+      
+        }
+    }
+}
